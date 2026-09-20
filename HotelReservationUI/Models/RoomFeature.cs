d00658a8356de328ac2443d6ae1d03057d75530b
Joshua Sunjaya <joshsunjaya@gmail.com>
2026-09-20 19:34:54 +0700

    Load room features from API

---
+++ HotelReservationUI/Models/RoomFeature.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelReservationUI.Models
+{
+    public class RoomFeature
+    {
+        public int Id { get; set; }
+        public int RoomTypeId { get; set; }
+        public string FeatureName { get; set; } = "";
+        public bool IsActive { get; set; }
+    }
+}
--- HotelReservationUI/Models/RoomType.cs
+++ HotelReservationUI/Models/RoomType.cs
@@ -10,5 +10,6 @@
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
+        public List<RoomFeature> Features { get; set; } = new();
     }
 }
